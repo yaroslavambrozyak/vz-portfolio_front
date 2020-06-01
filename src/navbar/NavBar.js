ab101048b8fe5644f84f006345ac93e727cd8bd4
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2020-06-01 14:58:43 +0300

    changed css selectors

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -5,32 +5,39 @@ import { NavLink } from 'react-router-dom';
 
 class NavBar extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            navOpened:false
+        this.state = {
+            navOpened: false
         }
     }
 
-    openSideNav(){
+    openSideNav = () => {
         this.setState(prevState => ({
             navOpened: !prevState.navOpened
-          }));
-    }
+        }));
+    };
+
+    scrollToTop = () => {
+        window.scrollTo(0, 0)
+    };
 
     render() {
         return (
             <div>
                 <div className={Style.side_nav}>
-                    <img src={Logo} alt='logo' className={Style.logo} />
-                    <div className={Style.nav_menu_button} onClick={()=>this.openSideNav()} ><span className={Style.nav_menu_button_text}>Menu</span>&#9776;</div>
-                    <div className={Style.scroll_top_link} onClick={() => window.scrollTo(0, 0)}>top</div>
+                    <img src={Logo} alt='logo' className={Style.side_nav__logo} />
+                    <div className={Style.side_nav_button} onClick={this.openSideNav} >
+                        <span className={Style.side_nav_button__text}>Menu</span>
+                        <span className={Style.side_nav_button__icon}>&#9776;</span>
+                    </div>
+                    <span className={Style.side_nav_scroll_top_button} onClick={this.scrollToTop}></span>
                 </div>
-                <div className={Style.side_nav_full + ' ' + (this.state.navOpened ? Style.side_nav_full_open: '')}>
-                    
-                        <NavLink className={Style.side_nav_full_link} to=''>Home</NavLink>
-                        <NavLink className={Style.side_nav_full_link} to='/hello'>Hello</NavLink>
-                    
+                <div className={Style.side_nav_extended + ' ' + (this.state.navOpened ? Style.side_nav_extended_opened : '')}>
+
+                    <NavLink className={Style.side_nav_extended__nav_link} to=''>Home</NavLink>
+                    <NavLink className={Style.side_nav_extended__nav_link} to='/hello'>Hello</NavLink>
+
                 </div>
             </div>
         );
