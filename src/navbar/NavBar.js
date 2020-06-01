9bbd33322f7d9aa15edb04cef571bf5b78f6d0c6
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2020-06-01 15:53:48 +0300

    specified nav animation for specific screens

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -8,8 +8,14 @@ class NavBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            navOpened: false
+            navOpened: false,
+            preloadFinished: false,
+            windowWidth: window.innerWidth
         }
+    }
+
+    componentDidMount(){
+        this.sideNavAnimationDisabler();
     }
 
     openSideNav = () => {
@@ -22,10 +28,14 @@ class NavBar extends React.Component {
         window.scrollTo(0, 0)
     };
 
+    sideNavAnimationDisabler = () => {
+        setTimeout(()=> this.setState({preloadFinished: true}), 1500);
+    }
+
     render() {
         return (
             <div>
-                <div className={Style.side_nav}>
+                <div className={Style.side_nav + ' ' + ((this.state.innerWidth > 769 &&!this.state.preloadFinished) ? Style.side_nav_animated : '')}>
                     <img src={Logo} alt='logo' className={Style.side_nav__logo} />
                     <div className={Style.side_nav_button} onClick={this.openSideNav} >
                         <span className={Style.side_nav_button__text}>Menu</span>
