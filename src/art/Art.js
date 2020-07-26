60c17c0354138c9f6c10df07f2b3e554102835ec
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2020-07-26 20:40:20 +0300

    config art page for mobile

--- src/art/Art.js
+++ src/art/Art.js
@@ -10,7 +10,9 @@ function Art(props) {
             <img className={style.art_image} src='https://cdnb.artstation.com/p/assets/images/images/019/215/045/large/vlx-zvarun-study-2.jpg?1562539028'></img>
             <img className={style.art_image} src='https://cdna.artstation.com/p/assets/images/images/018/762/520/large/vlx-zvarun-study-6s.jpg?1560620589'></img>
         </div>
-        <div className={style.art_text}>HEllo centrym centra HEllo centrym centra HEllo centrym centra HEllo centrym centra 
+        <div className={style.art_text}>HEllo centrym centra HEllo centrym centra HEllo centrym centra HEllo centrym centra
+        HEllo centrym centra HEllo centrym centra HEllo centrym centra HEllo centrym centra
+        HEllo centrym centra HEllo centrym centra HEllo centrym centra HEllo centrym centra 
         </div>
     </div>
 
