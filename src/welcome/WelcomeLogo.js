e67c5304e9e27b377fad0b294785969fef56262f
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2020-08-01 19:41:12 +0300

    added welcome page disappear timeout

--- src/welcome/WelcomeLogo.js
+++ src/welcome/WelcomeLogo.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Style from './WelcomeLogo.module.css';
 import Logo from './preloadLogo.gif';
 
 function WelcomeLogo(props) {
+
+    useEffect(()=>{
+        setDisapearTimer();
+    });
+
+    const setDisapearTimer = ()=>{
+      setTimeout(()=>{
+          props.history.push('/portfolio');
+      },2500)  
+    };
 
     return <div className={Style.welcome_page}>
         <img className={Style.welcome_page_logo} src={Logo} alt=''/>
