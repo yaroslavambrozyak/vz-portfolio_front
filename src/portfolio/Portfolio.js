b7ba369f1dd64015467ac1f461843f430b9fb741
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2020-08-02 15:27:06 +0300

    added transparent container on nav opened

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -5,12 +5,17 @@ import Art from './art/Art';
 
 class Portfolio extends React.Component {
 
-
     constructor(props) {
         super(props);
         this.state = {
             preloadFinished: false,
             disabled: false
+        };
+        this.styleDisplayNone={
+            display:'none'
+        };
+        this.styleDisplayBlock={
+            display:'block'
         };
     }
 
@@ -35,6 +40,7 @@ class Portfolio extends React.Component {
         return (
             <div>
                 <NavBar onNavBarOpened={()=>this.disableView()} preloadAnimationEnabled={true} />
+                <div style={this.state.disabled? this.styleDisplayBlock : this.styleDisplayNone} className={style.transparent_container}></div>
                 <div className={style.main_container + ' ' + (
                     (window.innerWidth > 769 && !this.state.preloadFinished) ? style.main_container_preload_animation : '')+ ' ' +
                     (window.innerWidth >769 && this.state.disabled? style.main_container_disabled: '')}>
