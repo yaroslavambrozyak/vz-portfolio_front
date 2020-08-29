0382d027079aff11ca2d64a64950566a50c020ae
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2020-08-30 02:36:59 +0300

    nav anim changes

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -5,23 +5,13 @@ import { Link } from 'react-router-dom';
 
 function NavBar(props) {
 
-    let [transitionAnimationFinished, setTransitionAnimationFisished] = useState(false);
-
     let sideNavigationCssClasses = `${Style.side_nav} 
     ${(window.innerWidth > 769 && props.logoPreloadFinished) ? Style.side_nav_width_animated : ''}`;
 
     let sideNavigationLogoCssClasses = `${Style.side_nav__logo} 
     ${props.navOpened ? Style.side_nav_extended__logo : ''}`;
 
-    let scrollTopButtonStyle = {display: !props.navOpened ? 'block' : 'none'};
-
-    useEffect(() => {
-        navAppearAnimationConfigurator();
-    }, []);
-
-    let navAppearAnimationConfigurator = () => {
-        setTimeout(() => { setTransitionAnimationFisished(true) }, 1500);
-    };
+    let scrollTopButtonStyle = { display: !props.navOpened ? 'block' : 'none' };
 
     let onScrollTopButtonClick = () => {
         window.scrollTo(0, 0)
@@ -34,9 +24,11 @@ function NavBar(props) {
                 <span className={Style.side_nav_button__text}>Menu</span>
                 <span className={Style.side_nav_button__icon}>&#9776;</span>
             </div>
-            <span style={scrollTopButtonStyle} className={Style.side_nav_scroll_top_button} onClick={onScrollTopButtonClick}></span>
+            {window.innerWidth > '769px' &&
+                <span style={scrollTopButtonStyle} className={Style.side_nav_scroll_top_button} onClick={onScrollTopButtonClick}></span>
+            }
         </div>
-        
+
     </div>
 };
 
