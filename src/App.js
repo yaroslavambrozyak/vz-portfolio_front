89c3a38b253a742d33bd1bd52bc8dc767f1dc995
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2020-09-27 19:02:39 +0300

    grid

--- src/App.js
+++ src/App.js
@@ -8,29 +8,47 @@ import NavBarContainer from './components/navbar/NavBarContainer';
 import Art from './components/art/Art';
 import PortfolioContainer from './components/portfolio/PortfolioContainer';
 import TransitionWrapper from './components/common/transition/TransitionWrapper';
+import Portfolio from './components/portfolio/Portfolio';
+import GridView from './components/admin/grid/GridView';
 
 function App() {
+
+  const Admin = ({ match }) => (
+    <React.Fragment>
+      <h1>admin bar</h1>
+      <GridView></GridView>
+    </React.Fragment>
+  );
+
+  const Portfilio = ({ match }) => (
+    <React.Fragment>
+      <WelcomeLogo />
+      <NavBarContainer />
+      <Route render={({ location }) => (
+        <TransitionWrapper location={location}>
+          <Switch location={location}>
+            <Route path='/art' render={() =>
+              <Art art={store.getState().art.art} prev={store.getState().art.prev} next={store.getState().art.next} />
+            }
+            />
+            <Route path={['/:category', '/']} render={() =>
+              <PortfolioContainer />
+            } />
+          </Switch>
+        </TransitionWrapper>
+      )}>
+      </Route>
+    </React.Fragment>
+  );
 
   return (
     <div>
       <BrowserRouter>
         <Provider store={store}>
-          <WelcomeLogo />
-          <NavBarContainer />
-          <Route render={({ location }) => (
-            <TransitionWrapper location={location}>
-              <Switch location={location}>
-                <Route path='/art' render={() =>
-                  <Art art={store.getState().art.art} prev={store.getState().art.prev} next={store.getState().art.next} />
-                }
-                />
-                <Route path={['/:category', '/']} render={() =>
-                  <PortfolioContainer />
-                } />
-              </Switch>
-            </TransitionWrapper>
-          )}>
-          </Route>
+          <Switch>
+            <Route path='/admin' component={Admin} />
+            <Route path='/' component={Portfilio} />
+          </Switch>
         </Provider>
       </BrowserRouter>
     </div >
---
+++ src/components/admin/grid/GridView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function GridView(props) {
+
+    return(
+        <div>hello</div>
+    )
+
+}
+
+export default GridView;
