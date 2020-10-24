85652c53206ebb496c0d330e7a7702052663648f
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2020-10-24 20:42:56 +0300

    before jenkins

--- src/api/axios-conf.js
+++ src/api/axios-conf.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
+    //baseURL: "http://ec2-100-25-196-44.compute-1.amazonaws.com:8080/api",
     baseURL: "http://localhost:8080/api",
     responseType: "json"
   });
