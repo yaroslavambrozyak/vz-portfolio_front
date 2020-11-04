d49036f8874666170284b75173de7c663d911a38
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2020-11-04 19:01:07 +0200

    added env

--- src/api/axios-conf.js
+++ src/api/axios-conf.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-
 export default axios.create({
-    //baseURL: "http://ec2-100-25-196-44.compute-1.amazonaws.com:8080/api",
-    baseURL: "http://localhost:8080/api",
+    baseURL: process.env.REACT_APP_API_URL,
     responseType: "json"
   });
