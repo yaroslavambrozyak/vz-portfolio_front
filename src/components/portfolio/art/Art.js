0310e02f57d37fddb3963ee32e003b2d51f7d18f
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2020-12-05 22:20:08 +0200

    added hover

--- src/components/portfolio/art/Art.js
+++ src/components/portfolio/art/Art.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import style from './Art.module.css';
 import { NavLink } from 'react-router-dom';
-import ProgressiveImage from '../../common/progressive/ProgressiveImage';
 
 function Art(props) {
 
     
     return <div className={`${style.image_container}`} style={{gridColumn:`span ${props.col}`, gridRow: `span ${props.row}`}} >
-        <ProgressiveImage smallResImage={props.smallImageUrl} highResImage={props.imageUrl}/>
+        <img className={style.image} src={props.imageUrl}/>
         <div className={style.image_overlay}>
             <div className={style.image_title}>{props.name}</div>
             <NavLink className={style.image_link} to={props.link}><span className={style.image_link_text}>VIEW</span></NavLink>
